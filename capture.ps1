999569a5ec47243fc6f692d61b5e20662ac81c92
Fernando Rojo <ferojo@microsoft.com>
2021-10-11 19:04:27 -0700

    add basic color adjustment to improve OCRsuccess rate and performance

--- capture.ps1
+++ capture.ps1
@@ -6,6 +6,8 @@ function screenshot([Drawing.Rectangle]$bounds) {
    $graphics = [Drawing.Graphics]::FromImage($bmp)
 
    $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+   # Applying color adjustmnet
+   $graphics.DrawImage($bmp,$drawingParallelogram,$drawingRectangle,[System.Drawing.GraphicsUnit]::Pixel , $attributes)
 
    $stream = New-Object IO.MemoryStream
    $bmp.Save($stream, ([system.drawing.imaging.imageformat]::png))
@@ -41,5 +43,22 @@ $bounds = [Drawing.Rectangle]::FromLTRB(
       $posOffset.x, 
       $posOffset.y + $posOffset.h
    )
+   
+# simple Color adjustment to try and isolate the coordinates to improve OCR success
+$drawingRectangle = new-object Drawing.Rectangle 0, 0, $bounds.Width, $bounds.Height
+# need to make this because the parameter sets for DrawImage requires them when using the color matrix
+$drawingParallelogram = @(
+   [Drawing.Point]::new(0, 0),
+   [Drawing.Point]::new($bounds.Width, 0),
+   [Drawing.Point]::new(0, $bounds.Height))
+[single[][]] $ColorCorrectionMatrix = 
+   (1.8, 1.8, 1.8, 0, 0),
+   (1.8,1.8, 1.8, 0, 0),
+   (3.0,3.0, 3.0, 0, 0),
+   (-0.45, -0.45, -0.45, 1, 0),
+   (-3.0,-3.0, -3.0, 0, 1)
+$colorMatrix = [System.Drawing.Imaging.ColorMatrix]::new($ColorCorrectionMatrix)
+$attributes  = [System.Drawing.Imaging.ImageAttributes]::new()
+$attributes.SetColorMatrix($colorMatrix)
 
 screenshot $bounds
